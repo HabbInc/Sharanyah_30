36680375f3209a2717452bd8c221079abde596ec
Sharah2001 <aranymahes01@gmail.com>
2026-01-30 13:08:09 +0530

    Implementing CRUD operations for database interactions

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,6 +1,12 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import {CARS} from "./cars.mock"
-import { retry } from 'rxjs';
+import {CARS} from "./cars.mock";
+import {Model} from "mongoose";
+import {InjectModel} from "@nestjs/mongoose";
+import {ICar} from "./interfaces/car.interface";
+import {CarDto} from "./car.dto";
+
+
+
 @Injectable()
 export class CarService {
     private cars= CARS;
@@ -13,31 +19,36 @@ export class CarService {
         return this.cars;
     }
 
-    public getCarById(id: number){
+    public getCarById(id: number):Promise<any>{
+        const carId = Number (id);
+        return new Promise((resolve)=>{
         const car= this.cars.find((car)=> car.id === id);
         if(car){
             throw new HttpException('Not Found', 404);
         }
-        return car;
+        return resolve(car);
+    });
 
     }
-     public deleteCarById(id: number){
-        const index = this.cars.findIndex((car)=> car.id === id);
+     public deleteCarById(id: number):Promise<any>{
+        const carId = Number (id);
+        return new Promise((resolve)=>{
+        const index = this.cars.findIndex((car)=> carId === id);
         if (index === -1){
             throw new HttpException('Not Found', 404);
         }
 
         this.cars.splice(index, 1);
-        return this.cars;
+        return resolve(this.cars);
+    })
      }
-     public putCarById(id:number, propertyName:string, propertyValue: string){
-        const index = this.cars.findIndex((car)=>car.id===id);
+     public putCarById(id:number, propertyName:string, propertyValue: string):Promise<any>{
+        const carId = Number (id);
+        return new Promise((resolve)=>{
+        const index = this.cars.findIndex((car)=>car.id===carId);
         if (index === -1){
             throw new HttpException('Not Found', 404);
         }
         this.cars[index][propertyName]= propertyValue;
-        return;
-     }
-
-
-}
+        return resolve (this.cars);
+     })}}
